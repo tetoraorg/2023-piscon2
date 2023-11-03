c3b2627a0134a2b29068192e78695cbb65603a9e
ras0q <ras0q@users.noreply.github.com>
2023-11-03 15:41:30 +0900

    :fire: remove Warnf

--- go/main.go
+++ go/main.go
@@ -1175,7 +1175,6 @@ func postIsuCondition(c echo.Context) error {
 	// TODO: 一定割合リクエストを落としてしのぐようにしたが、本来は全量さばけるようにすべき
 	dropProbability := 0.9
 	if rand.Float64() <= dropProbability {
-		c.Logger().Warnf("drop post isu condition request")
 		return c.NoContent(http.StatusAccepted)
 	}
 
