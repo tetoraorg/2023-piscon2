93ffa56e4084fdc26ef11e84504caf91c1f32c34
ras0q <ras0q@users.noreply.github.com>
2023-11-03 17:17:24 +0900

    :bug: 1s -> 0.5s

--- go/main.go
+++ go/main.go
@@ -349,7 +349,7 @@ func postInitialize(c echo.Context) error {
 	}
 
 	go func() {
-		for range time.Tick(1 * time.Second) {
+		for range time.Tick(500 * time.Millisecond) {
 			postIsuConditionMux.RLock()
 			if len(postIsuConditionArgs) == 0 {
 				continue
