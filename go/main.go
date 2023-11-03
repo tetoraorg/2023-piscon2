4c8f01b08fb56055782b9b734debaad75f344e88
tetoraorg <tetora@isucon.xyz>
2023-11-03 17:06:02 +0900

    fix

--- go/main.go
+++ go/main.go
@@ -1204,7 +1204,7 @@ func getTrend(c echo.Context) error {
 // ISUからのコンディションを受け取る
 func postIsuCondition(c echo.Context) error {
 	// TODO: 一定割合リクエストを落としてしのぐようにしたが、本来は全量さばけるようにすべき
-	dropProbability := 0.9
+	dropProbability := 0.5
 	if rand.Float64() <= dropProbability {
 		return c.NoContent(http.StatusAccepted)
 	}
