58501a238b8fd0d2ba2454ab0d5b26479269259d
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2023-11-03 16:00:38 +0900

    Revert "✨ PKに複合インデックスを適用"
    This reverts commit 53635b40e54cde4e65bf839f8f8405c881a47418.

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -24,7 +24,8 @@ CREATE TABLE `isu_condition` (
   `condition` VARCHAR(255) NOT NULL,
   `message` VARCHAR(255) NOT NULL,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY(`jia_isu_uuid`, `timestamp`)
+  INDEX idx_jia_isu_uuid_timestamp(`jia_isu_uuid`, `timestamp`),
+  PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
 CREATE TABLE `user` (
