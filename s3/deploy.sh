f5f803f85cbc887ef88c72e75abd23fb8f5881ad
tesso <48358052+tesso57@users.noreply.github.com>
2023-11-03 14:50:56 +0900

    ⚡️ add s3

---
+++ s3/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -aux
+
+sudo cp ./etc/nginx/nginx.conf /etc/nginx/nginx.conf
+sudo cp ./etc/mysql/mariadb.conf.d/50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
+
+export PPROTEIN_GIT_REPOSITORY=""
+export GOROOT=""
+export GOPROXY=https://proxy.golang.org,direct
+cd ~/webapp/go && go build -o isucondition
+
+# log
+sudo chmod +r /var/log/mysql
+sudo rm -rf /var/log/mysql/mysql-slow.log /var/log/nginx/access.log
+sudo touch /var/log/mysql/mysql-slow.log /var/log/nginx/access.log
+sudo chmod +r /var/log/mysql/mysql-slow.log /var/log/nginx/access.log /var/log/nginx/access.log
+sudo touch /var/log/mysql/mysql-slow.log /var/log/nginx/access.log
+sudo chmod +r /var/log/mysql/mysql-slow.log /var/log/nginx/access.log
+sudo chown mysql:mysql /var/log/mysql
+sudo chown mysql:mysql /var/log/mysql/mysql-slow.log
+
+
+sudo systemctl restart mysql.service
+sudo systemctl restart nginx.service
+sudo systemctl restart isucondition.go.service
+
+sudo mysql -uisucon -pisucon -e 'SET GLOBAL long_query_time = 0; SET GLOBAL slow_query_log = ON; SET GLOBAL slow_query_log_file = "/var/log/mysql/mysql-slow.log";'
