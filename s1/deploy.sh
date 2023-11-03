7584eb7ae5a96e01fe67b68bfd1d1317a20781c6
tesso <48358052+tesso57@users.noreply.github.com>
2023-11-03 14:10:43 +0900

    🐛 fix

--- s1/deploy.sh
+++ s1/deploy.sh
@@ -2,7 +2,8 @@
 
 set -aux
 
-sudo cp ../s1/etc/nginx/nginx.conf /etc/nginx/nginx.conf
+sudo cp ./etc/nginx/nginx.conf /etc/nginx/nginx.conf
+sudo cp ./etc/mysql/50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
 
 export PPROTEIN_GIT_REPOSITORY=""
 export GOROOT=""
