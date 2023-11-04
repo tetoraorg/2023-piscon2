34245ce3b7068fb04b8ccdcccdaa7145886797fa
ras0q <ras0q@users.noreply.github.com>
2023-11-05 00:02:39 +0900

    env.sh

--- s2/env.sh
+++ s2/env.sh
@@ -4,3 +4,7 @@ MYSQL_USER=isucon
 MYSQL_DBNAME=isucondition
 MYSQL_PASS=isucon
 POST_ISUCONDITION_TARGET_BASE_URL="http://isucondition-2.t.isucon.dev"
+
+PPROTEIN_GIT_REPOSITORY="/home/isucon/webapp"
+GOROOT=""
+GOPROXY=https://proxy.golang.org,direct
