c61b037122aa326c2a6cf9f3eaf13db7b76694bc
ras0q <ras0q@users.noreply.github.com>
2023-11-04 13:27:32 +0900

    restart nginx

--- s2/deploy.sh
+++ s2/deploy.sh
@@ -26,8 +26,7 @@ sudo chown mysql:mysql /var/log/mysql/mysql-slow.log
 
 
 sudo systemctl restart mysql.service
-# sudo systemctl restart nginx.service
-sudo systemctl stop nginx.service
+sudo systemctl restart nginx.service
 sudo systemctl restart isucondition.go.service
 
 sudo mysql -uisucon -pisucon -e 'SET GLOBAL long_query_time = 0; SET GLOBAL slow_query_log = ON; SET GLOBAL slow_query_log_file = "/var/log/mysql/mysql-slow.log";'
